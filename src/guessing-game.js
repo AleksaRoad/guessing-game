cfaa7587955be1e62c64fef5ad687ff30009cdbf
AleksaRoad <aleksaroad@gmail.com>
2023-02-16 18:57:49 +0300

    new answer

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.max = max;
+    this.min = min;
+  }
 
-    }
+  guess() {
+    return Math.round((this.max + this.min) / 2);
+  }
 
-    guess() {
+  lower() {
+    this.max = Math.round((this.max + this.min) / 2);
+    this.min = this.min;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = Math.round((this.max + this.min) / 2);
+    this.max = this.max;
+  }
 }
 
 module.exports = GuessingGame;
